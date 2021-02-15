98bf62931e24199ffdd44e7879a31b3669c948b9
stitch05 <74092012+stitch05@users.noreply.github.com>
2021-02-15 22:51:55 +0900

    修正④

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  password = Faker::Internet.password(min_length: 6)
+  password = 'a1' + Faker::Internet.password(min_length: 6)
   factory :user do
     nickname              { "yamada" }
     email                 { Faker::Internet.free_email }
