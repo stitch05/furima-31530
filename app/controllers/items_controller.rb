c2d5c84cc91438d65f42ffeea68b52954697b3d2
stitch05 <74092012+stitch05@users.noreply.github.com>
2021-03-24 20:57:11 +0900

    更新⑤

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @items = Item.includes(:delivery_fee, :user, :image).order("id DESC")
+    @items = Item.includes(:user).order("id DESC")
   end
   
   def new
